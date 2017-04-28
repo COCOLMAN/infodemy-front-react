924c658dc48ffe42affe05988314c553c0e9fe86
sol HYUN <solsol9057@gmail.com>
2017-04-28 16:00:00 +0900

    탑메뉴 로고 설정

--- src/components/common/TopMenu.js
+++ src/components/common/TopMenu.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 
 class TopMenu extends React.Component{
     componentWillMount(){
-	axios.defaults.baseURL = 'http://infodemy-dev-env.ap-northeast-2.elasticbeanstalk.com';
+ 	axios.defaults.baseURL = 'http://infodemy-dev-env.ap-northeast-2.elasticbeanstalk.com';
 	axios.defaults.headers.common['Authorization'] = 'Token ' +localStorage.getItem('token');
 	axios.get('/user/myinfo/')
 	    .then((response)=>{
@@ -20,6 +20,7 @@ class TopMenu extends React.Component{
 	      <div className="topbar-left">
 		<div className="text-center">
 		  <div className='logo'>
+		    <i className='md md-link'/>
 		    <span>Linkademy</span>
 		  </div>
 		</div>
